bb5e8e4c6d20b03daa119dc37c75875e4ca13799
Kripanshu Agrawal <kragrawal@egencia.com>
2023-05-15 22:20:10 +0530

    restaurant recommended using google api

--- restaurant-recommendation-bot/index.js
+++ restaurant-recommendation-bot/index.js
@@ -6,30 +6,40 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const GOOGLE_API_KEY = 'AIzaSyCYdj4YqU_KAaQzGh-yi_ykJhoAe_x4M7E'
 
 app.post('/restaurant-search', async (req, res) => {
+  console.log("Request body is " + JSON.stringify(req.body.queryResult));
   const { parameters } = req.body.queryResult;
-  const { name, cuisine, city } = parameters;
+  const { cuisine, location } = parameters;
+  let city = location['city']
 
   try {
-    const response = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json', {
+    const response = await axios.get('https://maps.googleapis.com/maps/api/place/autocomplete/json', {
       params: {
-        query: `${cuisine} restaurant ${name} in ${city}`,
-        location: '37.7749,-122.4194',
-        radius: 10000,
-        type: 'restaurant',
-        opennow: true,
-        key: 'AIzaSyCYdj4YqU_KAaQzGh-yi_ykJhoAe_x4M7E',
+        input: `${cuisine} restaurant in ${city}`,
+        // location: '37.7749,-122.4194',
+        // radius: 10000,
+        types: 'restaurant',
+        // opennow: true,
+        key: GOOGLE_API_KEY,
       },
     });
 
-    const { data } = response;
-    const { results } = data;
+    console.log("Google API REsponse is " + JSON.stringify(response.data));
+    const { predictions } = response.data;
 
-    if (results.length > 0) {
-      const restaurant = results[0];
-      const { name: restaurantName, formatted_address: address } = restaurant;
-      const message = `I recommend ${restaurantName} located at ${address} for ${cuisine} cuisine. It's open now and you can visit at ${time}.`;
+    if (predictions.length > 0) {
+      const place_id = predictions[0]['place_id'];
+      const place_detail = await axios.get('https://maps.googleapis.com/maps/api/place/details/json', {
+        params: {
+          place_id: place_id,
+          fields: 'formatted_address,name,geometry',
+          key: GOOGLE_API_KEY,
+        },
+      });
+      const { name: restaurantName, formatted_address: address } = place_detail.data.result;
+      const message = `I recommend ${restaurantName} for ${cuisine} cuisine located at ${address}.`;
       res.json({ fulfillmentText: message });
     } else {
       const message = `Sorry, I could not find any ${cuisine} restaurant near you that is open now. Please try again later or search for another cuisine type.`;
